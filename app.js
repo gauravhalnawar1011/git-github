b46b13073a3c511401659c226099d363a0e47652
Gaurav Halnawar <140076717+gauravhalnawar1011@users.noreply.github.com>
2025-12-10 17:20:33 +0530

    this for the demo

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById("loadBtn").addEventListener("click", () => {
+    fetch("http://13.233.227.192:3000/api/data")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("result").innerHTML = `
+                <h3>Data From Backend</h3>
+                <pre>${JSON.stringify(data, null, 2)}</pre>
+            `;
+        })
+        .catch(err => {
+            document.getElementById("result").innerHTML = "<p>Error fetching data!</p>";
+            console.error(err);
+        });
+});
